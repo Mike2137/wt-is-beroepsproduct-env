dc080f7f0343063d87912b9351f62b60c37869cb
Mike2137 <246374481+Mike2137@users.noreply.github.com>
2026-08-03 16:43:11 +0200

    Add all includes on pages

--- applicatie/BP3/menu.php
+++ applicatie/BP3/menu.php
@@ -1,7 +1,6 @@
 <?php
-require_once '../config.php';
-require_once '../includes/header.php';
-require_once '../includes/navigation.php';
+require_once 'includes/header.php';
+require_once 'includes/navigation.php';
 ?>
 
 <main>
--- applicatie/BP3/staff-orders.php
+++ applicatie/BP3/staff-orders.php
@@ -28,24 +28,6 @@ require_once 'includes/navigation.php';
 
 </main>
 
-<footer>
-    <section>
-        <h3>CONTACT</h3>
-        <p>123 Pizza Street, 6868PA, Arnhem</p>
-        <p>+31 6 12345678</p>
-    </section>
-
-    <section>
-        <h3>OPENING HOURS</h3>
-        <p>Mon - Thu: 16:00 - 22:00</p>
-        <p>Fri - Sun: 12:00 - 23:00</p>
-    </section>
-
-    <section>
-        <h3>Information</h3>
-        <a href="../pages/privacy-statement.html">Privacy Policy</a>
-    </section>
-</footer>
-</body>
-
-</html>
+<?php
+require_once 'includes/footer.php';
+?>

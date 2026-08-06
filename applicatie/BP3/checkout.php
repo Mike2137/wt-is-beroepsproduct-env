d9ff3412d1e6f473049121f2a43df846ae94203d
Mike2137 <246374481+Mike2137@users.noreply.github.com>
2026-08-06 19:29:31 +0200

    Add checkout functionality

--- applicatie/BP3/checkout.php
+++ applicatie/BP3/checkout.php
@@ -11,6 +11,100 @@ require_once 'includes/navigation.php';
 require_once 'includes/db_connection.php';
 
 $connection = createConnection();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $address = $_POST['address'];
+
+    $username = $_SESSION['username'];
+
+    $sql = "
+    SELECT first_name, last_name
+    FROM [User]
+    WHERE username = :username
+";
+
+    $statement = $connection->prepare($sql);
+
+    $statement->execute([
+        ':username' => $username
+    ]);
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $clientName = $user['first_name'] . " " . $user['last_name'];
+
+    $status = 1;
+
+    $sql = "
+    INSERT INTO Pizza_Order (
+        client_username,
+        client_name,
+        personnel_username,
+        datetime,
+        address,
+        status
+    )
+    VALUES (
+        :client_username,
+        :client_name,
+        :personnel_username,
+        GETDATE(),
+        :address,
+        :status
+    )
+";
+
+    $statement = $connection->prepare($sql);
+
+    $statement->execute([
+        ':client_username' => $username,
+        ':client_name' => $clientName,
+        ':personnel_username' => 'abrouwer',
+        ':address' => $address,
+        ':status' => $status
+    ]);
+
+    $sql = "
+    SELECT MAX(order_id) AS order_id
+    FROM Pizza_Order
+";
+
+    $statement = $connection->query($sql);
+
+    $order = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $orderId = $order['order_id'];
+
+    $sql = "
+    INSERT INTO Pizza_Order_Product (
+        order_id,
+        product_name,
+        quantity
+    )
+    VALUES (
+        :order_id,
+        :product_name,
+        :quantity
+    )
+";
+
+    $statement = $connection->prepare($sql);
+
+    foreach ($_SESSION['cart'] as $productName => $quantity) {
+
+        $statement->execute([
+            ':order_id' => $orderId,
+            ':product_name' => $productName,
+            ':quantity' => $quantity
+        ]);
+
+        unset($_SESSION['cart']);
+        header("Location: my-orders.php");
+        exit;
+    }
+}
+
+
 ?>
 
 <main>
